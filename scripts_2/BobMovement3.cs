dc563e5055ceb148641c97119346da86dd7c1b18
thobla <thorgalblanco@gmail.com>
2025-01-25 18:51:27 +0100

    (Feat) Simple collision logic, and bounce implemented

--- scripts_2/BobMovement3.cs
+++ scripts_2/BobMovement3.cs
@@ -8,13 +8,14 @@ public partial class BobMovement3 : CharacterBody2D
 	public const float Friction = 600f;
 	public const int radiusMax = 500;
 	public const int radiusMin = 30;
+	public const float bounceVar = 0.7f;
 	
 	private Vector2 InputDirection = Vector2.Zero;
 	
 	private Vector2 GetInpDir(){
 		Vector2 relativeVector = Vector2.Zero;
 		if(Input.IsActionPressed("cursor_left")){
-			Vector2 mousePosition = GetViewport().GetMousePosition();
+			Vector2 mousePosition = GetGlobalMousePosition();
 			Vector2 playerPosition = this.GlobalPosition;
 			float dist = 0;
 			relativeVector = new Vector2(playerPosition.X - mousePosition.X, playerPosition.Y - mousePosition.Y);
@@ -26,7 +27,6 @@ public partial class BobMovement3 : CharacterBody2D
 				relativeVector.X = relativeVector.X;
 				relativeVector.Y = relativeVector.Y;
 				relativeVector = relativeVector.Normalized() * scale;
-				GD.Print("scale: ", scale);
 			}
 			else{
 				relativeVector = Vector2.Zero;
@@ -44,12 +44,14 @@ public partial class BobMovement3 : CharacterBody2D
 		
 		
 		vel = InputDirection * MaxSpeed;
-		
+
+		var collision = MoveAndCollide((float)delta * new_vel);
+		if (collision != null){
+			GD.Print("Hello");
+		}
 		
 		if(InputDirection.X != 0){
 			new_vel.X = (float)Mathf.MoveToward(Velocity.X, vel.X, delta*Speed*scaleSpeedX);
-			GD.Print("scaleX", scaleSpeedX);
-			GD.Print("scaleY", scaleSpeedY);
 		}
 		else{
 			new_vel.X = (float)Mathf.MoveToward(Velocity.X, 0, delta*(Friction/MaxSpeed)*Speed);
@@ -60,12 +62,25 @@ public partial class BobMovement3 : CharacterBody2D
 		else{
 			new_vel.Y = (float)Mathf.MoveToward(Velocity.Y, 0, delta*(Friction/MaxSpeed)*Speed);
 		}
+		
 		SetVelocity(new_vel);
 	}
 	
 	public override void _PhysicsProcess(double delta){
-		UpdateVelocity(delta);
-
+		// We save velocity before collision, to bounce on it
+		Vector2 bounceVelocity = GetVelocity() * bounceVar;
 		MoveAndSlide();
+		if (GetSlideCollisionCount() > 0){
+			var collision = GetSlideCollision(0);
+			if (collision != null){
+				Velocity = bounceVelocity.Bounce(collision.GetNormal());
+			}
+			
+		}
+		UpdateVelocity(delta);
+	}
+
+	public void OnBodyEnteredKillZone(PhysicsBody2D body){
+		QueueFree();
 	}
 };
